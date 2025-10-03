5a65bedb9ded439957074d96ed4097c73a25e286
baopham1410 <161430592+baopham1410@users.noreply.github.com>
2025-10-03 16:06:03 +0700

    Migrate project from NetBeans to IntelliJ Maven

---
+++ src/main/java/org/example/tourbooking/Main.java
@@ -0,0 +1,12 @@
+package org.example.tourbooking;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Quoc Bao
+ */
+public class Main {
+    
+}
